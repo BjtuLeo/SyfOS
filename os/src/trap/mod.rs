607ecad7059a77a235c3ce06d5a470f93e9aba4b
BjtuLeo <23301012@bjtu.edu.cn>
2026-06-08 20:22:54 +0800

    实验六代码23301012孙轶凡

--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -1,43 +1,47 @@
-mod context;
-
-use crate::syscall::syscall;
-use crate::task::{
-    exit_current_and_run_next,
-    suspend_current_and_run_next,
-};
-use crate::timer::set_next_trigger;
-use core::arch::global_asm;
 use riscv::register::{
     mtvec::TrapMode,
     scause::{self, Exception, Interrupt, Trap},
-    sie, stval, stvec,
+    stval, stvec,
 };
-
+use core::arch::{global_asm, asm};
+use crate::task::{
+    exit_current_and_run_next, suspend_current_and_run_next,
+    current_user_token, current_trap_cx,
+};
+use crate::timer::set_next_trigger;
+use crate::config::{TRAP_CONTEXT, TRAMPOLINE};
+mod context;
 pub use context::TrapContext;
+use riscv::register::sie;
 
 global_asm!(include_str!("trap.S"));
 
 pub fn init() {
-    extern "C" {
-        fn __alltraps();
-    }
+    set_kernel_trap_entry();
+}
+
+fn set_kernel_trap_entry() {
     unsafe {
-        stvec::write(__alltraps as *const () as usize, TrapMode::Direct);
+        stvec::write(trap_from_kernel as *const () as usize, TrapMode::Direct);
     }
 }
 
-pub fn enable_timer_interrupt() {
-    unsafe { sie::set_stimer(); }
+fn set_user_trap_entry() {
+    unsafe {
+        stvec::write(TRAMPOLINE as *const () as usize, TrapMode::Direct);
+    }
 }
 
 #[no_mangle]
-pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
+pub fn trap_handler() -> ! {
+    set_kernel_trap_entry();
+    let cx = current_trap_cx();
     let scause = scause::read();
     let stval = stval::read();
     match scause.cause() {
         Trap::Exception(Exception::UserEnvCall) => {
             cx.sepc += 4;
-            cx.x[10] = syscall(cx.x[17], [cx.x[10], cx.x[11], cx.x[12]]) as usize;
+            cx.x[10] = crate::syscall::syscall(cx.x[17], [cx.x[10], cx.x[11], cx.x[12]]) as usize;
         }
         Trap::Exception(Exception::StoreFault) |
         Trap::Exception(Exception::StorePageFault) => {
@@ -56,5 +60,37 @@ pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
             panic!("Unsupported trap {:?}, stval = {:#x}!", scause.cause(), stval);
         }
     }
-    cx
+    trap_return();
+}
+
+#[no_mangle]
+pub fn trap_return() -> ! {
+    set_user_trap_entry();
+    let trap_cx_ptr = TRAP_CONTEXT;
+    let user_satp = current_user_token();
+    extern "C" {
+        fn __alltraps();
+        fn __restore();
+    }
+    let restore_va = __restore as *const () as usize - __alltraps as *const () as usize + TRAMPOLINE;
+    unsafe {
+        asm!(
+            "fence.i",
+            "jr {restore_va}",
+            restore_va = in(reg) restore_va,
+            in("a0") trap_cx_ptr,
+            in("a1") user_satp,
+            options(noreturn)
+        );
+    }
+}
+
+#[no_mangle]
+pub fn trap_from_kernel() -> ! {
+    panic!("a trap from kernel!");
+}
+pub fn enable_timer_interrupt() {
+    unsafe {
+        sie::set_stimer();
+    }
 }
---
+++ os/target/riscv64gc-unknown-none-elf/release/deps/zero-a923602b0487b107.d
@@ -0,0 +1,7 @@
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/zero-a923602b0487b107.d: /root/.cargo/registry/src/mirrors.ustc.edu.cn-38d0e5eb5da2abae/zero-0.1.3/src/lib.rs
+
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/libzero-a923602b0487b107.rlib: /root/.cargo/registry/src/mirrors.ustc.edu.cn-38d0e5eb5da2abae/zero-0.1.3/src/lib.rs
+
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/libzero-a923602b0487b107.rmeta: /root/.cargo/registry/src/mirrors.ustc.edu.cn-38d0e5eb5da2abae/zero-0.1.3/src/lib.rs
+
+/root/.cargo/registry/src/mirrors.ustc.edu.cn-38d0e5eb5da2abae/zero-0.1.3/src/lib.rs:
